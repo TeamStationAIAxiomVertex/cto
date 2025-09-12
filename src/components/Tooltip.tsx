c3b8fa4c3103e67026654d45f024b64ba7c4fa6b
Lonnie McRorey <lonnie@teamstation.io>
2025-09-12 17:05:50 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Unsupported Serv

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -5,7 +5,7 @@ type TooltipProps = {
   text: string;
 };
 
-export const Tooltip = ({ children, text }: TooltipProps) => {
+const Tooltip = ({ children, text }: TooltipProps) => {
   return (
     <span className="tooltip">
       {children}
@@ -13,3 +13,5 @@ export const Tooltip = ({ children, text }: TooltipProps) => {
     </span>
   );
 };
+
+export default Tooltip;
