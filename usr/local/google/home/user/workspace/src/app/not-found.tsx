870a72f96b19d404969c230b24a287796ceee5d9
Lonnie McRorey <lonnie@teamstation.io>
2025-10-02 20:51:30 +0000

    let’s give you the nuclear-safe bundle so Firebase Studio stops failing.

---
+++ usr/local/google/home/user/workspace/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <html>
+      <body>
+        <h1>Page Not Found</h1>
+        <p>This route doesn’t exist. Return to <a href="/">Home</a>.</p>
+      </body>
+    </html>
+  );
+}
