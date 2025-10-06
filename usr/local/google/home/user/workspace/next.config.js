bf8334747a6125ccb278c63d3d933f8086979c2d
Lonnie McRorey <lonnie@teamstation.io>
2025-10-06 06:28:29 +0000

    yo ufailed again
    ERROR: build step 3 "gcr.io/k8s-skaffold/pack" failed:

--- usr/local/google/home/user/workspace/next.config.js
+++ usr/local/google/home/user/workspace/next.config.js
@@ -1,14 +1,5 @@
+
 /** @type {import('next').NextConfig} */
-
-import withPWA from 'next-pwa';
-
-const pwaConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   output: "standalone",
 
@@ -26,4 +17,4 @@ const nextConfig = {
   productionBrowserSourceMaps: true
 };
 
-export default pwaConfig(nextConfig);
+export default nextConfig;
