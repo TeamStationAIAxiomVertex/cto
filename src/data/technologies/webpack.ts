e77f9203a5c36e74508f0d6c3ff2a4df7c626044
Lonnie McRorey <lonnie@teamstation.io>
2025-10-19 00:06:37 +0000

    more fixes to webpack

--- src/data/technologies/webpack.ts
+++ src/data/technologies/webpack.ts
@@ -12,24 +12,24 @@ const webpack: TechEntry = {
     pains: [
         {
             iconName: "AlertTriangle",
-            "pain": "Is your web application a mess of slow, bloated JavaScript?",
-            "problem": "In many organizations, the web application is a mess of slow, bloated JavaScript. This can lead to a user experience that is filled with long load times and a codebase that is a mess of environment-specific hacks and workarounds.",
-            "solution": "We vet for experts in **Webpack and the modern frontend stack**. Our engineers are proficient in using Webpack to build a fast, lean, and performant web application. This results in a codebase that is easy to understand and a user experience that is a joy to use.",
-            "kpi": "A Fast, Lean, and Performant Web Application"
+            pain: "Is your web application a mess of slow, bloated JavaScript?",
+            problem: "In many organizations, the web application is a mess of slow, bloated JavaScript. This can lead to a user experience that is filled with long load times and a codebase that is a mess of environment-specific hacks and workarounds.",
+            solution: "We vet for experts in **Webpack and the modern frontend stack**. Our engineers are proficient in using Webpack to build a fast, lean, and performant web application. This results in a codebase that is easy to understand and a user experience that is a joy to use.",
+            kpi: "A Fast, Lean, and Performant Web Application"
         },
         {
             iconName: "AlertTriangle",
-            "pain": "Is your application not taking full advantage of the modern web?",
-            "problem": "Many applications are not built to be truly modern. This can lead to a user experience that is slow, a codebase that is a mess of monolithic, hard-to-maintain code, and a web application that is not as rich as it could be.",
-            "solution": "Our engineers are masters of **Webpack and the modern web**. They are vetted on their ability to use modern web APIs and best practices to build a modern, fast, and feature-rich web application. This results in a codebase that is easy to understand and a user experience that is a joy to use.",
-            "kpi": "A Modern, Fast, and Feature-Rich Web Application"
+            pain: "Is your application not taking full advantage of the modern web?",
+            problem: "Many applications are not built to be truly modern. This can lead to a user experience that is slow, a codebase that is a mess of monolithic, hard-to-maintain code, and a web application that is not as rich as it could be.",
+            solution: "Our engineers are masters of **Webpack and the modern web**. They are vetted on their ability to use modern web APIs and best practices to build a modern, fast, and feature-rich web application. This results in a codebase that is easy to understand and a user experience that is a joy to use.",
+            kpi: "A Modern, Fast, and Feature-Rich Web Application"
         },
         {
             iconName: "AlertTriangle",
-            "pain": "Is your team not leveraging the full power of the frontend ecosystem?",
-            "problem": "The frontend ecosystem is vast and powerful, but many teams are not leveraging its full potential. This can lead to a codebase that is a mess of reinvented wheels and a user experience that is not as rich as it could be.",
-            "solution": "We look for engineers proficient with **the frontend ecosystem and the modern frontend stack**. They are vetted on their ability to build applications that are powered by the best of the frontend ecosystem. This results in a user experience that is a joy to use for everyone, and a codebase that is easy to maintain and test.",
-            "kpi": "An Application That is Powered by the Best of the Frontend Ecosystem"
+            pain: "Is your team not leveraging the full power of the frontend ecosystem?",
+            problem: "The frontend ecosystem is vast and powerful, but many teams are not leveraging its full potential. This can lead to a codebase that is a mess of reinvented wheels and a user experience that is not as rich as it could be.",
+            solution: "We look for engineers proficient with **the frontend ecosystem and the modern frontend stack**. They are vetted on their ability to build applications that are powered by the best of the frontend ecosystem. This results in a user experience that is a joy to use for everyone, and a codebase that is easy to maintain and test.",
+            kpi: "An Application That is Powered by the Best of the Frontend Ecosystem"
         }
     ],
     evaluation: [
